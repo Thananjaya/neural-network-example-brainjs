9ec9d9dac886302aa2e768c25597e2a036e4db14
Thananjaya <thananjaya091993@gmail.com>
2019-11-04 13:53:59 +0530

    neural network training for stock price prediction

--- stockPriceCalculator.js
+++ stockPriceCalculator.js
@@ -1,3 +1,40 @@
-const data = require('./stockPriceData.json')
+const data = require('./stockPriceData.json');
+const brain = require('brain.js');
 
-console.log(data)
+network = new brain.recurrent.LSTMTimeStep({
+    inputSize: 4,
+    hiddenLayers: [8, 8],
+    outputSize: 4
+})
+
+scaledDown = (step) => {
+    return {
+        open: step.open / 138,
+        high: step.high / 138,
+        low: step.low / 138,
+        close: step.close / 138
+    };
+}
+
+
+scaledUp = (step) => {
+    return {
+        open: step.open * 138,
+        high: step.high *138,
+        low: step.low * 138,
+        close: step.close * 138
+    };
+}
+
+const scaledData = data.map(scaledDown)
+
+const trainingData = [
+    scaledData.slice(0,5),
+    scaledData.slice(5,10),
+    scaledData.slice(10,15),
+    scaledData.slice(15,20)
+]
+
+network.train(trainingData, {learningRate: 0.005, errorThresh: 0.02});
+
+console.log(scaledUp(network.run(trainingData[0])))
